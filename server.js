7eb7f7bd6480c382f8d305a1349dbe5a6fb180a8
William Lepinski <willsp@gmail.com>
2013-04-19 15:46:31 -0300

    Missing http package

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var app = require('./app_base');
+var http = require('http');
 
 http.createServer(app).listen(app.get('port'), function(){
    console.log("Express server listening on port " + app.get('port') + ' - ' + app.get('env'));
